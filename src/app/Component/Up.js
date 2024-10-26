02023c984fdef15a1027390a6514ef8aee65a7e1
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2024-10-26 20:46:25 +0300

    df

--- src/app/Component/Up.js
+++ src/app/Component/Up.js
@@ -10,7 +10,7 @@ function Up() {
         } else {
             document.getElementById("up").style.scale = '0';
         }
-    }
+    } 
     return (
         <IconButton id="up" onClick={() => window.scrollTo(0, 0)} className="up" style={{ position: 'fixed', bottom: '0', backgroundColor: '#404040', right: '15px', borderRadius: '10px 10px 0 0', padding: '0', zIndex: '100', opacity: '0.8', height: '35px', transition: '0.3s', scale: '0' }}>
             <ArrowDropUpOutlined style={{ color: 'white', fontSize: '40px' }} />
